384e61af46066e02303e633e9c361800ed8970b2
Marcos Capistrano <mcapistrano50@gmail.com>
2025-06-05 19:20:24 -0300

    beggining bike controller

--- main.c
+++ main.c
@@ -1,23 +1,32 @@
 #include <stdio.h>
+#include <time.h>
 #include <math.h>
 #include "raylib.h"
+#include "raymath.h"
 #include "game/area.h"
 
 #define WORLD_WIDTH 19
 #define WORLD_HEIGHT 19
 
+typedef struct Motorcycle
+{
+    Vector2 position;
+    float speed;
+    float angle; // Direction the bike is facing (in degrees)
+    float lean;  // Lean angle (affects turn)
+} Motorcycle;
 
 int main()
 {
     const int screenWidth = 800;
     const int screenHeight = 450;
 
+    int isMounted = 0;
+
     InitWindow(screenWidth, screenHeight, "2d-topdown");
 
+    Motorcycle bike = {(Vector2){400, 280}, 0, 0, 0};
     Rectangle player = {400, 280, 40, 40};
-
-    Rectangle tiles[WORLD_WIDTH][WORLD_HEIGHT] = {0};
-    Color tilesColors[WORLD_WIDTH][WORLD_HEIGHT] = {0};
 
     Area_load(1);
 
@@ -27,7 +36,7 @@ int main()
     camera.rotation = 0.0f;
     camera.zoom = 1.0f;
 
-    SetTargetFPS(2000);
+    SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
@@ -43,13 +52,46 @@ int main()
         else if (IsKeyDown(KEY_S))
             player.y += 2;
 
+        if (IsKeyDown(KEY_F))
+        {
+            if (Vector2Distance((Vector2){player.x, player.y}, (Vector2){bike.position.x, bike.position.y}) < 500)
+            {
+                isMounted = !isMounted;
+            }
+        }
+
         camera.target = (Vector2){player.x + 20, player.y + 20};
 
-        if(player.x > 200)
+        if (isMounted)
         {
-            Area_load(1);
-        }  else {
-            Area_load(2);
+            if (IsKeyDown(KEY_W))
+                bike.speed += 0.2f; // Accelerate
+            if (IsKeyDown(KEY_S))
+                bike.speed -= 0.2f; // Brake/reverse
+            if (IsKeyDown(KEY_A))
+                bike.lean -= 1.0f; // Lean left
+            if (IsKeyDown(KEY_D))
+                bike.lean += 1.0f; // Lean right
+
+            // Clamp lean angle
+            if (bike.lean > 45)
+                bike.lean = 45;
+            if (bike.lean < -45)
+                bike.lean = -45;
+
+            // Apply lean to steering angle
+            bike.angle += bike.lean * 0.05f;
+
+            // Apply speed to position
+            bike.position.x += cosf(DEG2RAD * bike.angle) * bike.speed;
+            bike.position.y += sinf(DEG2RAD * bike.angle) * bike.speed;
+
+            // Friction
+            bike.speed *= 0.98f;
+            bike.lean *= 0.9f;
+
+            player.x = bike.position.x - (player.width/2);
+            player.y = bike.position.y - (player.height/2);
         }
 
         // DRAW
@@ -60,8 +102,24 @@ int main()
 
             BeginMode2D(camera);
             {
-                        Area_draw();
+                Area_draw();
                 DrawRectangleRec(player, RED);
+
+                Vector2 frontWheel = {
+                    bike.position.x + cosf(DEG2RAD * bike.angle) * 20,
+                    bike.position.y + sinf(DEG2RAD * bike.angle) * 20};
+                Vector2 backWheel = {
+                    bike.position.x - cosf(DEG2RAD * bike.angle) * 20,
+                    bike.position.y - sinf(DEG2RAD * bike.angle) * 20};
+                Vector2 left = {
+                    bike.position.x + cosf(DEG2RAD * (bike.angle - 90)) * 10,
+                    bike.position.y + sinf(DEG2RAD * (bike.angle - 90)) * 10};
+                Vector2 right = {
+                    bike.position.x + cosf(DEG2RAD * (bike.angle + 90)) * 10,
+                    bike.position.y + sinf(DEG2RAD * (bike.angle + 90)) * 10};
+
+                DrawLineV(backWheel, frontWheel, BLACK);
+                DrawCircleV(bike.position, 3, RED);
             }
             EndMode2D();
 
@@ -76,7 +134,7 @@ int main()
             Vector2 mouseWorld = GetScreenToWorld2D(GetMousePosition(), camera);
             DrawText(TextFormat("Mouse Screen: [%i, %i]", GetMouseX(), GetMouseY()), 22, 22, 22, BLACK);
             DrawText(TextFormat("Mouse World: [%f, %f]", mouseWorld.x, mouseWorld.y), 22, 55, 22, BLACK);
-            DrawText(TextFormat("FPS: %.0f", 1/GetFrameTime()), 22, 77, 22, BLACK);
+            DrawText(TextFormat("FPS: %.0f", 1 / GetFrameTime()), 22, 77, 22, BLACK);
             /* ----------------------------------------- */
         }
         EndDrawing();
